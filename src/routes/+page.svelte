33e5fc056f718fab36640ad944a9231d0e73ac28
Kyrylo <kirillusichenko@gmail.com>
2023-10-29 22:21:38 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>Hello everyone, I will show how Svelte's basic features work.</h1>
+
+	<h2>
+		To navigate between pages, you can use the navigation menu at the top or the
+		<strong>"Next"</strong> button.
+	</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
